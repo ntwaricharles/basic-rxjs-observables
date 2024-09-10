fe6b1c20861da2b76f8046b0540d216255984baa
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-10 11:46:59 +0200

    Error Handling and Debugging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { SearchComponent } from './components/search/search.component';
-import { BookSearchComponent } from './components/book-search/book-search.component';
-import { BookListComponent } from './components/book-list/book-list.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { ErrorDebuggingComponent } from './error-debugging/error-debugging.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RxjsComponent,
-    SearchComponent,
-    BookSearchComponent,
-    BookListComponent,
     ErrorDebuggingComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
