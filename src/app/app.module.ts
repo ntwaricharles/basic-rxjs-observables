c6d8680d2d643fb17877719478ef46fdacd6c401
ntwaricharles <charles.ntwari@amalitechtraining.org>
2024-09-10 11:39:05 +0200

    RxJs search and Data Combination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
 import { SearchComponent } from './components/search/search.component';
 import { BookSearchComponent } from './components/book-search/book-search.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ErrorDebuggingComponent } from './error-debugging/error-debugging.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RxjsComponent,
     SearchComponent,
     BookSearchComponent,
     BookListComponent,
-    ErrorDebuggingComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
